550175acc0bf7e6a744e5fa6f8b4ab1c977c96f7
lofoo <lofootballer2@mail.ru>
2021-04-26 22:01:33 +0700

    Great success bjs-Diplom

--- public/homePage.js
+++ public/homePage.js
@@ -1,16 +1,19 @@
 'use strict';
 const logoutButton = new LogoutButton();
 
+//This is Logout
 logoutButton.action = () => {
     ApiConnector.logout(response => {
         if (response.success) return location.reload();
     })
 }
 
+//This is current user info
 ApiConnector.current(response => {
     if (response.success) ProfileWidget.showProfile(response.data);
 });
 
+//This is current rate board
 const ratesBoard = new RatesBoard();
 ratesBoard.getStocks = () => ApiConnector.getStocks(response => {
     if (response.success) {
@@ -20,3 +23,58 @@ ratesBoard.getStocks = () => ApiConnector.getStocks(response => {
 });
 ratesBoard.getStocks();
 setInterval(ratesBoard.getStocks, 60000);
+
+//This is MONEeeeeeey
+const moneyManager = new MoneyManager();
+moneyManager.addMoneyCallback = (data) => {
+    ApiConnector.addMoney(data, response => {
+        moneyManager.setMessage(response.success, response.error);
+        if (response.success) ProfileWidget.showProfile(response.data);
+    })
+}
+
+moneyManager.conversionMoneyCallback = (data) => {
+    ApiConnector.convertMoney(data, response => {
+        moneyManager.setMessage(response.success, response.error);
+        if (response.success) ProfileWidget.showProfile(response.data);
+    })
+}
+
+moneyManager.sendMoneyCallback = (data) => {
+    ApiConnector.transferMoney(data, response => {
+        moneyManager.setMessage(response.success, response.error);
+        if (response.success) ProfileWidget.showProfile(response.data);
+    })
+}
+
+//This is Favorite
+const favoritesWidget = new FavoritesWidget();
+ApiConnector.getFavorites(response => {
+    if (response.success) {
+        favoritesWidget.clearTable();
+        favoritesWidget.fillTable(response.data);
+        moneyManager.updateUsersList(response.data);
+    }
+})
+
+favoritesWidget.addUserCallback = (data) => {
+    ApiConnector.addUserToFavorites(data, response => {
+        favoritesWidget.setMessage(response.success, response.error);
+        if (response.success) {
+            favoritesWidget.clearTable();
+            favoritesWidget.fillTable(response.data);
+            moneyManager.updateUsersList(response.data);
+        }
+    })
+}
+
+favoritesWidget.removeUserCallback = (data) => {
+    ApiConnector.removeUserFromFavorites(data, response => {
+        favoritesWidget.setMessage(response.success, response.error);
+        if (response.success) {
+            favoritesWidget.clearTable();
+            favoritesWidget.fillTable(response.data);
+            moneyManager.updateUsersList(response.data);
+        }
+    })
+}
